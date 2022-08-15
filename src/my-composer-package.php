09b66d403858fc4f242fdaf1d73187eda1b963f2
Akram Ghaleb <46090581+akramdev-ye@users.noreply.github.com>
2022-08-15 22:06:26 +0300

    Update my-composer-package.php

--- src/my-composer-package.php
+++ src/my-composer-package.php
@@ -7,7 +7,7 @@ class Greeting
 {
     public function greet(String $sName)
     {
-        return 'Hi ' . $sName . '! How are you doing today?';
+        return 'Hi ' . $sName . '! How are you doing today? ';
     }
 }
 
